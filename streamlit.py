fc8b23d8ca94783f8420310fdef260005feeff8a
lozo6 <99729303+lozo6@users.noreply.github.com>
2022-11-18 11:49:43 -0500

    added data

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,25 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv('data/LoL_Champion_Data.csv')
+riot_data = 'data/LoL_Champion_Data.csv'
+twitter_data = 'data/twitter_stocks.csv'
 
-print(df.columns)
+def get_data(data):
+    return pd.read_csv(data)
+
+# reading dataset
+league_df = get_data(riot_data)
+twitter_df = get_data(twitter_data)
+
+# creating dashboard title
+st.header('League of Legends & Twitter Stocks Data')
+
+# adding dashboard caption
+st.caption('History of League of Legends & Twitter Stocks Data')
+
+# displaying each dataframe
+if st.checkbox('League of Legends Data'):
+    st.dataframe(league_df)
+
+if st.checkbox('Twitter Stock'):
+    st.dataframe(twitter_data)
